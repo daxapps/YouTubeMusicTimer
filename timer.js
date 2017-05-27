8c82c2f993899f11435119a3172cb0f5fc399800
Jason Crawford <daxapps777@gmail.com>
2017-05-27 15:25:41 -0500

    Pause and restart timer

--- timer.js
+++ timer.js
@@ -14,15 +14,32 @@ function startSec(){
       }else if (tmin == 0 && tsec == -1) {
             tsec = 59;
       }else if (tmin == 0 && tsec == 0) {
-            alert("Done!");
+            // alert("Done!");
+
       }
       time.innerHTML = tmin + "m " + tsec + "s";
 }
-function initiate(){
-      // tmin = document.getElementById("hrs").value;
-      tmin = document.getElementById("min").value;
-      tsec = document.getElementById("sec").value;
-      time.innerHTML = tmin + ":" + tsec; 
-      x = setInterval(startSec, 1000);
-
+function startTimer(){
+  $('#startBtn').hide()
+  $('#stopBtn').show()
+  tmin = document.getElementById("min").value;
+  tsec = document.getElementById("sec").value;
+  time.innerHTML = tmin + ":" + tsec; 
+  x = setInterval(startSec, 1000);
 }
+
+function stopTimer(){
+  $('#startBtn').show()
+  $('#stopBtn').hide()
+  x = clearTimeout(x)
+}
+
+$('#startBtn').on("click", function(e) {
+  startTimer();
+})
+
+$('#stopBtn').on("click", function(e) {
+  e.preventDefault();
+  stopTimer();
+})
+
